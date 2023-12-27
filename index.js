d12ee5ff1e4b7788685c049cdc18239450001ccf
Ingrid Kathlen <ingrika6@gmail.com>
2023-12-27 12:10:35 -0300

    commit de desafio

---
+++ index.js
@@ -0,0 +1,26 @@
+let userName = "João"
+let xp = 50000
+
+
+if (xp < 1000){
+    console.log("O Herói de nome " + userName + " está no nível Bronze")
+}
+    else if(xp > 2001 && 5000){
+        console.log("O Herói de nome " + userName + " está no nível Prata")
+    }
+    else if(xp > 5001 && 7000){
+        console.log("O Herói de nome " + userName + " está no nível Ouro")
+    }
+    else if(xp > 7001 && 8000){
+        console.log("O Herói de nome " + userName + " está no nível Platina")
+    }
+    else if(xp > 8001 && 9000){
+        console.log("O Herói de nome " + userName + " está no nível Ascendente")
+    }
+    else if(xp > 9001 && 10000){
+        console.log("O Herói de nome " + userName + " está no nível Imortal")
+    }
+    else{
+        console.log ("O Herói de nome " + userName + " está no nível Radiante")
+    }
+
